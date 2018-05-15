ab3203e0b13751bbc69d56410e4703a8398354be
KevinDeley <36673196+KevinDeley@users.noreply.github.com>
2018-05-15 16:03:44 +0200

    Add files via upload

---
+++ UNO_KEVIN_MENU_JOYSTICK/UNO_KEVIN_MENU_JOYSTICK.ino
@@ -0,0 +1,144 @@
+/*UNO-module (VIVES): ontvangen van data van andere arduino via softwarematig aangelegd kanaal op digitale pinnen 10, 11
+                      om menu te wijzigen en deze data te splitsen van de seriële data op de USB.
+   BELANGRIJK: DIGpin 10 = RX
+               DIGpin 11 = TX
+               Aansluiting --> pin 10 (uno1) naar pin 11 (uno2)
+                               pin 11 (uno1) naar pin 10 (uno2)
+               !! Beide uno's op gelijke massa plaatsen !!
+  JOYSTICK: aansluiting --> GND - GND
+                            5V - 5V
+                            VRx - pin 0
+  LCD : aansluiting --> GND - GND
+  |||I2C communicatie|||Vcc - 5V
+                        SDA - SDA
+                        SCL - SCL              
+*/               
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+#define I2C_ADDR 0x27
+#include <SoftwareSerial.h>
+
+//                         I2C,RS,RW,EN,D4,D5,D6,D7,BL,ACTIVE)
+LiquidCrystal_I2C lcd(I2C_ADDR, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+
+char heartRateMenu [1] = "A";
+char tempMenu [1] = "B";
+char xMenu [1] = "C";
+char yMenu [1] = "D";
+char zMenu [1] = "E";
+int menuPosition = 0;
+
+SoftwareSerial menuSerial(10, 11); //(RX, TX)
+
+const int X_pin = 0; // analog pin connected to X output
+
+//int xValue = analogRead(X_pin);
+
+void setup()
+{ 
+ lcd.begin(16,2);
+ //lcd.clear();
+ Serial.begin(9600);
+ menuSerial.begin(4800);
+}
+
+void loop()
+{
+  int xValue = analogRead(X_pin);
+
+  if (xValue <100)
+  {
+     menuPosition -= 1;
+     if (menuPosition < 0)
+     {
+        menuPosition = 4; 
+     }
+  } else if (xValue >900)
+  {
+     menuPosition += 1;
+     if (menuPosition > 4)
+     {
+        menuPosition = 0;
+      }
+  }
+  
+  switch (menuPosition)
+  {
+    case 0:
+      heartrate();
+      menuSerial.write(heartRateMenu, 1);      
+    break;
+    case 1:  
+      temperature();
+      menuSerial.write(tempMenu, 1);
+    break;
+    case 2:
+      xAxis();
+      menuSerial.write(xMenu, 1);
+    break;
+    case 3:
+      yAxis();
+      menuSerial.write(yMenu, 1);
+    break;
+    default:
+      zAxis();
+      menuSerial.write(zMenu, 1); 
+    break;
+  }
+}
+
+void heartrate()
+{
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("< Z-Axis");
+  lcd.setCursor(3, 1);
+  lcd.print("Temperature");
+  lcd.setCursor(15, 1);
+  lcd.print(">");
+  delay(400);
+}
+void temperature()
+{
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("< Heartrate");
+  lcd.setCursor(3, 1);
+  lcd.print("X-Axis");
+  lcd.setCursor(15, 1);
+  lcd.print(">");
+  delay(400);
+}
+void xAxis()
+{
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("< Temperature");
+  lcd.setCursor(3, 1);
+  lcd.print("Y-Axis");
+  lcd.setCursor(15, 1);
+  lcd.print(">");
+  delay(400);
+}
+void yAxis()
+{
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("< X-Axis");
+  lcd.setCursor(3, 1);
+  lcd.print("Z-Axis");
+  lcd.setCursor(15, 1);
+  lcd.print(">");
+  delay(400);
+}
+void zAxis()
+{
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("< Y-Axis");
+  lcd.setCursor(3, 1);
+  lcd.print("Heartrate");
+  lcd.setCursor(15, 1);
+  lcd.print(">");
+  delay(400);
+}
---
+++ UNO_VIVES_SENSORS/UNO_VIVES_SENSORS.ino
@@ -0,0 +1,164 @@
+/*UNO-module (VIVES): verwerken en serieel doorsturen van sensor data naar USB
+                      ontvangen van data van andere arduino via softwarematig aangelegd kanaal op digitale pinnen 10, 11
+                      om menu te wijzigen en deze data te splitsen van de seriële data op de USB.
+   BELANGRIJK: DIGpin 10 = RX
+               DIGpin 11 = TX
+               Aansluiting --> pin 10 (uno1) naar pin 11 (uno2)
+                               pin 11 (uno1) naar pin 10 (uno2)
+               !! Beide uno's op gelijke massa plaatsen !! 
+   TEMPERATUURSENSOR: aansluiting --> 3,3V - 3,3V  !!!GEEN 5V!!!
+                                      GND - GND
+                                      SDA - pin A4
+                                      SCL - pin A5
+   ACCELEROMETER: aansluiting --> 3.3V - 3.3V !!!GEEN 5V!!!
+                                  GND - GND
+                                  SDA - pin A4
+                                  SCL - pin A5
+   HEARTRATE: aansluiting -->  Vcc - 5V
+                               GND - GND
+                               S(signal) - pin A0                                  
+*/               
+
+#include <LiquidCrystal.h>
+#include <Wire.h>
+#define I2C_ADDR 0x27
+#include <SparkFunTMP102.h>
+#include <SparkFun_MMA8452Q.h> 
+#include <SoftwareSerial.h>
+
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+TMP102 sensor0(0x48); 
+MMA8452Q accel;
+int incomingBytes = 0;
+const int ALERT_PIN = A3;
+int adc_key_in;
+int Signal;  
+int BeatsPM;
+int Threshold = 550;
+int PulseSensorPurplePin = 0;
+
+const int HEARTRATE_CODE = 65;
+const int TEMPERATURE_CODE = 66;
+const int X_AXIS_CODE = 67;
+const int Y_AXIS_CODE = 68;
+const int Z_AXIS_CODE = 69;
+
+SoftwareSerial menuSerial(10, 11); //(RX, TX)
+
+void setup()
+{
+  Serial.begin(115200);
+  menuSerial.begin(4800);
+  
+  pinMode(10, INPUT);
+  pinMode(11, OUTPUT);  
+  pinMode(ALERT_PIN,INPUT);
+  
+  lcd.begin(16,2);
+  accel.init();
+    
+  sensor0.begin(); 
+  sensor0.setConversionRate(3);                     // Detectiesnelheid 3 = 8Hz
+  sensor0.setExtendedMode(0); 
+}
+
+void loop()
+{ 
+  incomingBytes = menuSerial.read();
+  initiateAccelero(); 
+  writeDataToSerial();
+  
+  if (menuSerial.available()>0) 
+  {  
+     incomingBytes = menuSerial.read();       
+     if (incomingBytes == TEMPERATURE_CODE)
+     {
+      temperatureData();      
+     } else if (incomingBytes == X_AXIS_CODE)
+     {
+      acceleroX();
+     } else if (incomingBytes == Y_AXIS_CODE)
+     {
+      acceleroY();
+     } else if (incomingBytes == Z_AXIS_CODE)
+     {
+      acceleroZ();
+     } else if (incomingBytes == HEARTRATE_CODE)
+     {
+      heartRate();
+     }
+  } 
+}
+void writeDataToSerial()
+{
+  Signal = analogRead(PulseSensorPurplePin);
+  BeatsPM = (1.0/Signal) * 60.0 * 1000;
+  float temperature = sensor0.readTempC();
+  Serial.print(BeatsPM + String("~"));
+  Serial.print(temperature + String("~"));
+  Serial.print((accel.cx, 2) + String("~"));
+  Serial.print((accel.cy, 2) + String("~"));
+  Serial.println(accel.cz, 2);
+}
+
+void initiateAccelero()
+{
+  if(accel.available())
+  {
+    accel.read();
+    delay(50);
+  }
+}
+
+void heartRate()
+{
+  Signal = analogRead(PulseSensorPurplePin);
+  BeatsPM = (1.0/Signal) * 60.0 * 1000;  
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Heartrate:");
+  lcd.setCursor(0, 1);
+  lcd.print("BPM: ");
+  lcd.setCursor(5, 1);
+  lcd.print(BeatsPM);
+}
+
+void temperatureData()
+{
+  float temperature = sensor0.readTempC();
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Temperature:");
+  lcd.setCursor(0, 1);
+  lcd.print(temperature);
+  lcd.setCursor(6, 1);
+  lcd.print("degrees C");
+}
+
+void acceleroX()
+{
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("X-axis:"),
+  lcd.setCursor(0, 1);
+  lcd.print(accel.cx, 3);
+}
+
+void acceleroY()
+{
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Y-axis:"),
+  lcd.setCursor(0, 1);
+  lcd.print(accel.cy, 3);
+}
+
+void acceleroZ()
+{
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Z-axis:"),
+  lcd.setCursor(0, 1);
+  lcd.print(accel.cz, 3);
+}
+
